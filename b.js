4c565b2bdcdf2b5916aec310a6d04c27d2a4149a
Cxelts21 <redolaiacc@gmail.com>
2023-12-17 16:43:23 +0700

    Initial commit

---
+++ b.js
@@ -0,0 +1,13 @@
+const blogs = [
+    {
+        website: "",
+        image: "",
+        color: "bg-blue-500",
+        category: "mẹ mày",
+        title: "mẹ mày béo",
+        description: "She could fall into the grand canyon and not fit in",
+        author: "a",
+        date: "1/1/2001",
+        read_time: "5 mins"
+    }
+]
